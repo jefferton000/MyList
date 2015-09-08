f0a142e99d8de9c4417362f9e4aafbefc82fda04
jefferton000 <jefferton@gmail.com>
2015-09-07 22:15:39 -0700

    Create AList.java

---
+++ AList.java
@@ -0,0 +1,13 @@
+/** List implementation using the ListInterface.
+ *  Implement the class and it's methods once you have written your tests.
+ *  (Supposed to be good pracetice)
+ */
+public class AList implements ListInterface {
+  public AList()
+  {
+    //default constructor  
+  }
+  
+  // class methods
+  
+}
